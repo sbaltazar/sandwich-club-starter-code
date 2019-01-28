37306e56764ef0e47b8629751343f538dae86100
Sebastian Baltazar <sebastian.baltazar@antaramining.cl>
2019-01-28 11:24:02 -0300

    feat: Added visualization of details of the sandwiches
    The DetailActivity now shows the information of each sandwich from the list
    included in the starting code. If the information is too large, the app is
    able to scroll across the data.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,6 +16,12 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    // UI elements to access
+    private TextView mSandwichOrigin;
+    private TextView mSandwichAlsoKnown;
+    private TextView mSandwichIngredients;
+    private TextView mSandwichDescription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +29,22 @@ public class DetailActivity extends AppCompatActivity {
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
 
+        // If compileSdk from app gradle is >= 26 it's not necessary to cast the views, otherwise
+        // casting is mandatory
+        mSandwichOrigin = findViewById(R.id.origin_tv);
+        mSandwichAlsoKnown = findViewById(R.id.also_known_tv);
+        mSandwichIngredients = findViewById(R.id.ingredients_tv);
+        mSandwichDescription = findViewById(R.id.description_tv);
+
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
 
-        int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+        int position = -1;
+        if (intent != null) {
+            position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+        }
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
             closeOnError();
@@ -43,7 +60,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +73,44 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    /**
+     *
+     * Populates the UI with the sandwich details
+     *
+     * @param sandwich Selected sandwich
+     */
+    private void populateUI(Sandwich sandwich) {
 
+        // Instead of showing a blank label, a message for the user is displayed
+        // We could use ternary operation to simplify the code.
+
+        if (sandwich.getAlsoKnownAs().isEmpty()) {
+            mSandwichAlsoKnown.setText("N/A");
+        } else {
+            for (String otherName : sandwich.getAlsoKnownAs()) {
+                mSandwichAlsoKnown.append(" - " + otherName + "\n");
+            }
+        }
+
+        if (sandwich.getPlaceOfOrigin().isEmpty()) {
+            mSandwichOrigin.setText("N/A");
+        } else {
+            mSandwichOrigin.setText(sandwich.getPlaceOfOrigin());
+        }
+
+        if (sandwich.getDescription().isEmpty()) {
+            mSandwichDescription.setText("N/A");
+        } else {
+            mSandwichDescription.setText(sandwich.getDescription());
+
+        }
+
+        if (sandwich.getIngredients().isEmpty()) {
+            mSandwichIngredients.setText("N/A");
+        } else {
+            for (String ingredient : sandwich.getIngredients()) {
+                mSandwichIngredients.append(" - " + ingredient + "\n");
+            }
+        }
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,65 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
+    public static Sandwich parseSandwichJson(String json)  {
+
+        try {
+            JSONObject jsonSandwich = new JSONObject(json);
+
+            // Name object of the sandwich
+            JSONObject jsonName = jsonSandwich.getJSONObject("name");
+
+            String mainName = jsonName.getString("mainName");
+
+            // Equivalent names of the sandwich
+            JSONArray jsonOtherNames = jsonName.getJSONArray("alsoKnownAs");
+
+            List<String> otherNames = new ArrayList<>();
+
+            // Getting each name from JSON Array
+            for (int i = 0; i < jsonOtherNames.length(); i++) {
+                String ingredient = jsonOtherNames.getString(i);
+                otherNames.add(ingredient);
+            }
+
+            // Place of the origin of the sandwich
+            String placeOfOrigin = jsonSandwich.getString("placeOfOrigin");
+
+            // Description of the sandwich
+            String description = jsonSandwich.getString("description");
+
+            // Image URL of the sandwich
+            String imageUrl = jsonSandwich.getString("image");
+
+            // Ingredients JSON of the sandwich
+            JSONArray jsonIngredients = jsonSandwich.getJSONArray("ingredients");
+
+            List<String> ingredients = new ArrayList<>();
+
+            // Getting each ingredients from JSON Array
+            for (int i = 0; i < jsonIngredients.length(); i++) {
+                String ingredient = jsonIngredients.getString(i);
+                ingredients.add(ingredient);
+            }
+
+            Sandwich sandwich = new Sandwich(mainName, otherNames, placeOfOrigin, description,
+                    imageUrl, ingredients);
+
+            return sandwich;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
